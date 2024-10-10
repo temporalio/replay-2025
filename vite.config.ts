810256afc6b1975062302b413eb451d2a99f8486
Steve Kinney <hello@stevekinney.net>
2024-10-10 14:26:52 -0600

    Add support for YAML

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import yaml from '@modyfi/vite-plugin-yaml';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), yaml()],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
