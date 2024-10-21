4038c1e69ea4342065e5f0ee66c3b5c805e271dc
Steve Kinney <hello@stevekinney.net>
2024-10-21 12:09:12 -0600

    Add Tailwind typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography';
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
@@ -20,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 } satisfies Config;
