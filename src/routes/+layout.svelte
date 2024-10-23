8be32b9fcfe5cfed092e0f34660f9155fbc16cd3
Steve Kinney <hello@stevekinney.net>
2024-10-23 14:06:23 -0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,15 @@
 </script>
 
 <main>
-  <div class="container my-10 flex flex-col items-center gap-4 font-display text-4xl uppercase">
+  <a
+    href="/"
+    class="container my-10 flex flex-col items-center gap-4 font-display text-4xl uppercase"
+  >
     <img src={logo} alt="Temporal logo" class="w-24" />
     <h1 class="text-6xl lg:text-9xl">Replay</h1>
     <p>March 3–5, 2025</p>
     <p>London, UK</p>
-  </div>
+  </a>
 
   <slot />
   <CookieConsent />
