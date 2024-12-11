7dbea5deb1ca55f6a431633bc817071ef3f04408
Steve Kinney <hello@stevekinney.net>
2024-12-10 19:55:03 -0700

    Add link for previous years

--- src/lib/components/previous-year.svelte
+++ src/lib/components/previous-year.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-  import { Button } from '$components/button';
+  import links from '$content/links.yaml';
   import highlights from '$assets/highlights.png';
 </script>
 
 <section>
   <div class="flex flex-col items-center">
     <img src={highlights} alt="Highlights" class="block w-full md:w-fit" />
-    <Button intent="secondary" label="Play 2024 Reel" />
+    <a
+      href={links.replay2024videos}
+      target="_blank"
+      class="flex gap-2 bg-blue-to-mint px-8 py-4 font-sans text-lg font-bold uppercase text-black hover:cursor-pointer hover:text-pink-dark"
+    >
+      <span>Play 2024 Reel</span>
+      <span>▶️</span>
+    </a>
   </div>
 </section>
