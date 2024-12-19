af4785b7c1995eac1058c604a152bbc4e898ebd4
Steve Kinney <hello@stevekinney.net>
2024-12-18 19:25:06 -0700

    Update to-type.js

--- scripts/generate-contentful/content-type/to-type.js
+++ scripts/generate-contentful/content-type/to-type.js
@@ -1,12 +1,12 @@
 /**
  * Converts a Contentful field to a TypeScript type.
  * @param {import('contentful').ContentTypeField} field
- * @param {ContentType} contentType
+ * @param {import('./index').ContentType} contentType
  * @returns {string}
  */
 export const toType = (field, contentType) => {
   let result = `EntryFieldTypes.${field.type}`;
-  const values = getValidations(field);
+  const values = getEmumValues(field);
 
   if (field.type === 'Array') {
     return `EntryFieldTypes.Array<${toType(field.items, contentType)}>`;
@@ -22,31 +22,39 @@ export const toType = (field, contentType) => {
 
       for (const type of field.validations) {
         if (type.linkContentType) {
-          const linkedType = contentType.generator.getContentType(type.linkContentType[0]);
+          const types = type.linkContentType.map((id) => contentType.generator.getContentType(id));
 
-          if (linkedType) {
+          for (const linkedType of types) {
+            if (!linkedType) continue;
             contentType.imports.add(linkedType);
             links.push(linkedType.skeleton.name);
           }
         }
       }
 
-      return `EntryFieldTypes.EntryLink<${links.join(' | ')}>`;
+      return `EntryFieldTypes.EntryLink<${union(links)}>`;
     }
   }
 
   if (values.length) {
-    result += `<${values.map((value) => `'${value}'`).join(' | ')}>`;
+    result += `<${union(values.map((value) => `'${value}'`))}>`;
   }
 
   return result;
 };
 
 /**
+ * Creates a union type from an array of types.
+ * @param {string[]} types
+ */
+const union = (types) => types.join(' | ');
+
+/**
+ * Get the validation values for a field.
  * @param {import('contentful').ContentTypeField} field
  * @returns {string[]}
  */
-const getValidations = (field) => {
+const getEmumValues = (field) => {
   if (!field.validations) return [];
   for (const validation of field.validations) {
     if (validation.in) {
