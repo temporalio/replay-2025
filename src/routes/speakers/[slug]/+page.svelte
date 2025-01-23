ec62d6840a91e8caac9af6b45ddd8cb443f469cc
grace <grace.gardner@temporal.io>
2025-01-22 17:53:48 -0700

    add stuff

--- src/routes/speakers/[slug]/+page.svelte
+++ src/routes/speakers/[slug]/+page.svelte
@@ -1,29 +1,25 @@
 <script lang="ts" module>
-  import { twMerge as merge } from 'tailwind-merge';
-  import { speaker, portrait, speakerName, type SpeakerProps } from '$components/speaker/variants';
+  import { type SpeakerProps } from '$components/speaker/variants';
   import Stonehenge from '$components/stonehenge.svelte';
 </script>
 
 <script lang="ts">
   import Breadcrumb from '$components/breadcrumb.svelte';
 
-  const { image, bio, jobTitle }: SpeakerProps = $props();
+  const { image, bio, jobTitle, name }: SpeakerProps = $props();
 </script>
 
 <Stonehenge />
-<div class={merge(speaker({ type }), 'mt-24 flex flex-col justify-start px-6')}>
+<div class={'mt-24 flex flex-col justify-start px-6'}>
   <Breadcrumb text="Return to speakers" href="/speakers" />
-  <h1 class={merge(speakerName({ type }), 'text-2xl font-bold text-white')}>{name}</h1>
+  <h1 class={'text-2xl font-bold text-white'}>{name}</h1>
   <div class="border-grey flex w-full flex-col items-center gap-4 border bg-black p-6">
     <div>
       <p class="text-white">{jobTitle}</p>
       {#if bio}
         <p class="font-lilac prose">{bio}</p>
       {/if}
-      <div
-        class={portrait}
-        style="background-image: url({image}?w=720&h=648&fm=webp);"
-      ></div>
+      <div style="background-image: url({image}?w=720&h=648&fm=webp);"></div>
     </div>
   </div>
 </div>
