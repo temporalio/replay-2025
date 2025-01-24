730c153293f07df07f17125061ab540002498147
Katie Toler <katie.toler@temporal.io>
2025-01-24 12:09:30 -0700

    Update faq

--- src/lib/components/faq/faq-item.svelte
+++ src/lib/components/faq/faq-item.svelte
@@ -1,26 +1,11 @@
 <script lang="ts">
-  import { slide } from 'svelte/transition';
-  import Icon from '../icon/icon.svelte';
-  import Text from '../text/text.svelte';
-  import RichText from '../rich-text/rich-text.svelte';
-
-  export let question: string;
-  export let answer: string | undefined = undefined;
-  export let entityId: string | undefined = undefined;
-
-  let expanded = false;
+  import type { FAQ, Questions } from '$lib/contentful/models/faq';
+  export let content: Questions;
+  console.log('AHHHHH', content);
+  $: ({ entityId, question, answer } = content.fields);
 </script>
 
 <div class="border-blue-gray-300 flex w-full flex-col border-b py-4">
-  <button class="flex w-full justify-between text-left" on:click={() => (expanded = !expanded)}>
-    <Text {entityId} fieldId="question" as="p" variant="body-24">{question}</Text>
-    <Icon name={expanded ? 'hyphen' : 'add'} />
-  </button>
-  {#if expanded}
-    <div transition:slide class="py-4">
-      <RichText background="none" text={answer} {entityId} fieldId="answer">
-        <slot />
-      </RichText>
-    </div>
-  {/if}
+  <p>{question}</p>
+  <p>{answer}</p>
 </div>
--- src/lib/components/faq/faq.svelte
+++ src/lib/components/faq/faq.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-  import type { Questions } from '$lib/contentful';
-  import type { FAQ, QuestionAndAnswer } from './faq-types';
-  export let content: FAQ;
-  console.log('cxontetn', content);
-  $: ({ title, entityId, questionsAndAnswers } = content);
+  import type { FAQ, Questions } from '$lib/contentful/models/faq';
+  export let content: Questions[];
+  import FaqItem from './faq-item.svelte';
+  export let title: string;
+
+  $: ({ entityId, questionsAndAnswers } = content);
 </script>
 
-<Grid as="div" layout="columns" {background}>
-  <Box as="div" class="flex flex-col items-center gap-8">
-    <Text {entityId} fieldId="title" variant="display-68">{title}</Text>
-    <div class="w-full">
-      {#each questionsAndAnswers as { question, answer, entityId }}
-        <FaqItem {question} {answer} {entityId} />
-      {/each}
-    </div>
-  </Box>
-</Grid>
+<div class="flex flex-col items-center gap-8">
+  <div {entityId} fieldId="title" variant="display-68">{title}</div>
+  <div class="w-full">
+    {#each questionsAndAnswers}
+      <FaqItem content={questionsAndAnswers} />
+    {/each}
+  </div>
+</div>
