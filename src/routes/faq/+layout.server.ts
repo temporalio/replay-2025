7766e22ba59ecfc97a711cfcf5f417d67f0e3cb1
Katie Toler <katie.toler@temporal.io>
2025-01-27 12:38:46 -0700

    Clean up syntax on faq entries server

--- src/routes/faq/+layout.server.ts
+++ src/routes/faq/+layout.server.ts
@@ -3,28 +3,21 @@ import { compileMarkdown } from '$lib/utilities/compile-markdown';
 import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types.js';
 
-
 export const prerender = true;
 export const load: LayoutServerLoad = async () => {
   const content = await getFAQEntries();
 
-  const faqEntries = await Promise.all((content.items ?? []).map(async (entry) => {
-    
-    
-    ...content, 
-    fields: {
-      ...content.fields,
-      answer: await compileMarkdown(content.fields.answer ?? '', true),
-    },
-
-    return entry}));
-
-
-
+  const faqEntries = await Promise.all(
+    (content.items ?? []).map(async (entry) => ({
+      ...entry,
+      fields: {
+        ...entry.fields,
+        answer: await compileMarkdown(entry.fields.answer ?? '', true),
+      },
+    })),
+  );
 
   // TO DO: We need to use the compileMarkdown function to format each of the answers for the questions in each category. KT
 
-  // const markdown = await compileMarkdown(faqEntries ?? '', true);
-
-  return { FAQ: faqEntries.items };
+  return { FAQ: faqEntries };
 };
