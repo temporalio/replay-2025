e62dd725889b59f23d0926955d616bf78f0f3cbb
Katie Toler <katie.toler@temporal.io>
2025-01-28 10:50:52 -0700

    Fix type error"

--- src/lib/components/button/button.svelte
+++ src/lib/components/button/button.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import { twMerge as merge } from 'tailwind-merge';
   import { button, type ButtonProps } from './variants';
+
   const { href, label, intent = 'primary', class: className, ...props }: ButtonProps = $props();
+
+  const safeClassName = typeof className === 'string' ? className : '';
 </script>
 
-<a {href} class={merge(button({ intent }), className)} {...props}>
+<a {href} class={merge(button({ intent }), safeClassName)} {...props}>
   {label}
 </a>
