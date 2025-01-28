6a905bd65eb5073b4e54092face4acf0acf67e68
grace <grace.gardner@temporal.io>
2025-01-28 14:58:20 -0700

    changing the server thingy

--- src/routes/schedule/[slug]/+page.server.ts
+++ src/routes/schedule/[slug]/+page.server.ts
@@ -1,8 +1,24 @@
-import { getSessionSlugs } from '$lib/contentful/client';
-import type { EntryGenerator } from './$types';
+import { getSessionEntries, getSpeakerEntries } from '$lib/contentful/index.js';
+import { compileMarkdown } from '$lib/utilities/compile-markdown';
+import type { PageServerLoad } from './$types.js';
+import { error } from '@sveltejs/kit';
+export const prerender = true;
 
-export const entries: EntryGenerator = async () => {
-  const slugs = await getSessionSlugs();
 
-  return slugs;
+export const load: PageServerLoad = async ({ params }) => {
+  const sessions = await getSessionEntries({ 'fields.slug': params.slug });
+  const [session] = sessions.items;
+
+  if (!session) throw error(404, 'Session not found');
+
+  const speakers = await getSpeakerEntries({ 'fields.speakers.sys.id': session.sys.id });
+  const description = await compileMarkdown(session.fields.description);
+
+  return {
+    speaker : session.fields.speakers,
+    description,
+
+    talks: talks.items,
+    hasSpeakers: speakers.items.length > 0,
+  };
 };
