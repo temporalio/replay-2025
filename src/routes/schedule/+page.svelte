213b63882031e9635dd695fba2d61598cc41f5ad
Katie Toler <katie.toler@temporal.io>
2025-01-28 10:36:03 -0700

    Fix schedule type error

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts" module>
   import AbstractBanner from '$components/abstract-banner.svelte';
   import Schedule from '$components/schedule/schedule.svelte';
+  import type { TimeSlot } from '$lib/contentful';
 </script>
 
 <script lang="ts">
@@ -18,9 +19,17 @@
 <section class="relative flex flex-col items-center bg-grid">
   <section class="section flex flex-col justify-start gap-8 p-6 pb-16 pt-36">
     <h1 class="hero-subtitle text-4xl uppercase text-white lg:text-5xl">Schedule</h1>
-
-    <Schedule title="Day 1: Mar 3, 2025" slots={dayOneSlots} />
-    <Schedule title="Day 2: Mar 4, 2025" slots={dayTwoSlots} />
-    <Schedule title="Day 3: Mar 5, 2025" slots={dayThreeSlots} />
+    <Schedule
+      title="Day 1: Mar 3, 2025"
+      slots={dayOneSlots as TimeSlot<'WITHOUT_UNRESOLVABLE_LINKS', never>[]}
+    />
+    <Schedule
+      title="Day 2: Mar 4, 2025"
+      slots={dayTwoSlots as TimeSlot<'WITHOUT_UNRESOLVABLE_LINKS', never>[]}
+    />
+    <Schedule
+      title="Day 3: Mar 5, 2025"
+      slots={dayThreeSlots as TimeSlot<'WITHOUT_UNRESOLVABLE_LINKS', never>[]}
+    />
   </section>
 </section>
