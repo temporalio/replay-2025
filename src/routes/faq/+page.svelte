a258ff9de89e9486f2f4b52cb1957af5b955648d
Steve Kinney <hello@stevekinney.net>
2025-01-27 18:04:47 -0800

    Simplify FAQ

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,16 +1,16 @@
-<script>
-  import Faq from '$components/faq/faq.svelte';
-  const { data } = $props();
-  const [questions] = data.FAQ;
-  const { generalQuestions, ticketingQuestions, miscQuestions } = questions.fields;
+<script lang="ts">
   import AbstractBanner from '$components/abstract-banner.svelte';
+  import FaqItem from '$components/faq/faq-item.svelte';
+
+  const { data } = $props();
+  const { title, categories } = data;
 </script>
 
 <svelte:head>
   <title>Frequently Asked Questions: Replay Conference</title>
   <meta
     name="description"
-    content="Faq for Replay, an annual conference hosted by Temporal dedicated to the platforms, practices, and trends in backend software engineering."
+    content="Frequently Asked Questions for Replay, an annual conference hosted by Temporal dedicated to the platforms, practices, and trends in backend software engineering."
   />
   <meta property="og:title" content="FAQ: Replay Conference" />
   <meta
@@ -24,21 +24,21 @@
 </svelte:head>
 
 <AbstractBanner />
-<section class="relative flex flex-col items-center bg-grid">
-  <div class="mt-36 max-w-[940px]">
-    <div class="hero-subtitle flex flex-col items-start p-4 uppercase tracking-wide text-white">
-      Frequently Asked Questions
-    </div>
-    {#if generalQuestions}
-      <Faq title="General Questions" content={generalQuestions} />
-    {/if}
 
-    {#if ticketingQuestions}
-      <Faq title="Ticketing Questions" content={ticketingQuestions} />
-    {/if}
+<section class="relative flex flex-col items-center bg-grid py-36">
+  <div class="container">
+    <h2 class="hero-subtitle uppercase tracking-wide text-white">{title}</h2>
 
-    {#if miscQuestions}
-      <Faq title="Misc Questions" content={miscQuestions} />
-    {/if}
+    {#each categories as { title, questions }}
+      <section>
+        <h3 id="title" class="font-josefin mb-8 text-2xl uppercase text-teal">
+          {title}
+        </h3>
+
+        {#each questions as question}
+          <FaqItem question={question.question} answer={question.answer} />
+        {/each}
+      </section>
+    {/each}
   </div>
 </section>
