83511f0d5aa618401c62b970de4da13a20840c8b
Katie Toler <katie.toler@temporal.io>
2025-01-29 12:00:17 -0700

    Add a bit of padding to bottom of session detail box

--- src/routes/schedule/[slug]/+page.svelte
+++ src/routes/schedule/[slug]/+page.svelte
@@ -4,10 +4,10 @@
   const { session, speakers, date, hasSpeakers, description } = data;
 </script>
 
-<section class="relative flex flex-col items-center justify-center gap-8 bg-grid px-6 pt-24">
+<section class="relative flex flex-col items-center justify-center gap-8 bg-grid px-6 py-24">
   <div class="flex max-w-6xl flex-col justify-start gap-8">
     <Breadcrumb text="Return to schedule" href="/schedule" />
-    <h1 class="font-afacad text-5xl uppercase text-white">{session.title}</h1>
+    <h1 class="font-afacad text-5xl uppercase text-white max-xs:text-2xl">{session.title}</h1>
     <div class="flex flex-col items-start pr-10 text-green md:block">{date}</div>
 
     <div class="flex w-full flex-col gap-4 border border-grey bg-black p-6">
