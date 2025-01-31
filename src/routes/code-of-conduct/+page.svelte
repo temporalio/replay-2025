828b86aef647da25ee57846780537d2c8f90a386
Steve Kinney <hello@stevekinney.net>
2025-01-31 14:34:52 -0700

    Update +page.svelte

--- src/routes/code-of-conduct/+page.svelte
+++ src/routes/code-of-conduct/+page.svelte
@@ -16,5 +16,251 @@
     label="Harassment Complaint Form"
   />
 
-  <CodeOfConduct class="prose prose-invert" />
+  <div class="prose">
+    <p>
+      This Code of Conduct may be revised at any time by Temporal, and the terms are non-negotiable.
+    </p>
+    <p>
+      Temporal is dedicated to providing a harassment-free experience for participants on all our
+      platforms and events, whether they are held virtually or in person.
+    </p>
+    <h2 id="participants-contributors-and-temporal-staff">
+      Participants, Contributors, and Temporal Staff
+    </h2>
+    <p>
+      As participants, contributors, and Temporal staff, in the interest of fostering an open and
+      welcoming community, we pledge to respect all people who contribute through reporting issues,
+      posting feature requests, updating documentation, submitting pull requests or patches, asking
+      questions on our support platforms, and other activities.
+    </p>
+    <p>
+      We are committed to making participation in Temporal projects a harassment-free experience for
+      everyone, regardless of level of experience, gender, gender identity and expression, sexual
+      orientation, disability, personal appearance, body size, race, ethnicity, age, religion, or
+      nationality.
+    </p>
+    <p>Examples of unacceptable behavior by participants include:</p>
+    <ul>
+      <li>The use of sexualized language or imagery</li>
+      <li>Personal attacks</li>
+      <li>Trolling or insulting/derogatory comments</li>
+      <li>Public or private harassment</li>
+      <li>
+        Publishing others’ private information, such as physical or electronic addresses, without
+        explicit permission
+      </li>
+      <li>Other unethical or unprofessional conduct</li>
+    </ul>
+    <p>
+      Temporal staff have the right and responsibility to remove, edit, or reject comments, commits,
+      code, wiki edits, issues, and other contributions that are not aligned to this Code of
+      Conduct. By adopting this Code of Conduct, Temporal staff commit themselves to fairly and
+      consistently applying these principles to every aspect of managing this project.
+    </p>
+    <p>
+      This code of conduct applies both within project spaces and in public spaces when an
+      individual is representing the project or its community.
+    </p>
+    <p>
+      Instances of abusive, harassing, or otherwise unacceptable behavior in Temporal may be
+      reported by contacting us via <a href="mailto:conduct@temporal.io">conduct@temporal.io</a> or using
+      the Harassment Complaint Form button at the top of this page.
+    </p>
+    <h2 id="online-discussion">Online Discussion</h2>
+    <h3 id="civilized-place-for-public-discussion">Civilized Place for Public Discussion</h3>
+    <p>
+      Please treat our discussion forums with the same respect you would a public park. It is a
+      shared community resource — a place to share skills, knowledge, and interests through ongoing
+      conversation.
+    </p>
+    <p>
+      These are not hard and fast rules—they are guidelines to aid the human judgment of our
+      community and keep this a clean and well-lit place for civilized public discourse.
+    </p>
+    <h3 id="improve-the-discussion">Improve the Discussion</h3>
+    <p>
+      Help us make this a great place for discussion by always working to improve the discussion in
+      some way, however small. If you are not sure your post adds to the conversation, think over
+      what you want to say and try again later.
+    </p>
+    <p>
+      The topics discussed here matter to us, and we want you to act as if they matter to you, too.
+      Be respectful of the topics and the people discussing them, even if you disagree with some of
+      what is being said.
+    </p>
+    <p>
+      One way to improve the discussion is by discovering discussions that are already happening.
+      Spend time browsing the topics here before replying or starting your own, and you’ll have a
+      better chance of meeting others who share your interests.
+    </p>
+    <h3 id="be-agreeable-even-when-you-disagree">Be Agreeable, Even When You Disagree</h3>
+    <p>
+      You may wish to respond to something by disagreeing with it. That’s fine. But remember to
+      criticize ideas, not people. Please avoid:
+    </p>
+    <ul>
+      <li>Name-calling</li>
+      <li>Ad hominem attacks</li>
+      <li>Responding to a post’s tone instead of its actual content</li>
+      <li>Knee-jerk contradiction</li>
+    </ul>
+    <p>Instead, provide reasoned counterarguments that improve the conversation.</p>
+    <h3 id="your-participation-counts">Your Participation Counts</h3>
+    <p>
+      The conversations we have here set the tone for every new arrival. Help us influence the
+      future of this community by choosing to engage in discussions that make this forum an
+      interesting place to be — and avoiding those that do not.
+    </p>
+    <p>
+      Discourse provides tools that enable the community to collectively identify the best (and
+      worst) contributions: bookmarks, likes, flags, replies, edits, and so forth. Use these tools
+      to improve your own experience, and everyone else’s, too.
+    </p>
+    <p>Let’s leave our community better than we found it.</p>
+    <h3 id="if-you-see-a-problem-flag-it">If You See a Problem, Flag It</h3>
+    <p>
+      Moderators have special authority; they are responsible for the forums. But so are you. With
+      your help, moderators can be community facilitators, not just janitors or police.
+    </p>
+    <p>
+      When you see bad behavior, don’t reply. It encourages the bad behavior by acknowledging it,
+      consumes your energy, and wastes everyone’s time. Just flag it. If enough flags accrue, action
+      will be taken, either automatically or by moderator intervention.
+    </p>
+    <p>
+      In order to maintain our community, moderators reserve the right to remove any content and any
+      user account for any reason at any time. Moderators do not preview new posts; the moderators
+      and site operators take no responsibility for any content posted by the community.
+    </p>
+    <h3 id="always-be-civil">Always Be Civil</h3>
+    <p>Nothing sabotages a healthy conversation like rudeness:</p>
+    <ul>
+      <li>
+        Be civil. Don’t post anything that a reasonable person would consider offensive, abusive, or
+        hate speech.
+      </li>
+      <li>Keep it clean. Don’t post anything obscene or sexually explicit.</li>
+      <li>
+        Respect each other. Don’t harass or grief anyone, impersonate people, or expose their
+        private information.
+      </li>
+      <li>Respect our forum. Don’t post spam or otherwise vandalize the forum.</li>
+    </ul>
+    <p>
+      These are not concrete terms with precise definitions — avoid even the appearance of any of
+      these things. If you’re unsure, ask yourself how you would feel if your post was featured on
+      the front page of the New York Times.
+    </p>
+    <p>
+      Our discussion forums are public. Keep the language, links, and images safe for family and
+      friends.
+    </p>
+    <h3 id="keep-it-tidy">Keep It Tidy</h3>
+    <p>
+      Make the effort to put things in the right place, so that we can spend more time discussing
+      and less cleaning up. So:
+    </p>
+    <ul>
+      <li>Don’t start a topic in the wrong category.</li>
+      <li>Don’t cross-post the same thing in multiple topics.</li>
+      <li>Don’t post no-content replies.</li>
+      <li>Don’t divert a topic by changing it midstream.</li>
+      <li>Don’t sign your posts — every post has your profile information attached to it.</li>
+    </ul>
+    <p>
+      Rather than posting “+1” or “Agreed”, use the Like button or 👍 reaction. Rather than taking
+      an existing topic in a radically different direction:
+    </p>
+    <ul>
+      <li>Discourse: use Reply as a Linked Topic.</li>
+      <li>Slack: post a new thread with a link to the existing thread.</li>
+    </ul>
+    <h3 id="post-only-your-own-stuff">Post Only Your Own Stuff</h3>
+    <p>
+      You may not post anything digital that belongs to someone else without permission. You may not
+      post descriptions of, links to, or methods for stealing someone’s intellectual property
+      (software, video, audio, images) or breaking any other law.
+    </p>
+    <h3 id="powered-by-you">Powered by You</h3>
+    <p>
+      Discourse is operated by your friendly local staff and you, the community. If you have any
+      further questions about how things should work, open a new topic in the site feedback category
+      and let’s discuss! If there’s a critical or urgent issue that can’t be handled by a meta topic
+      or flag, contact us via the staff page.
+    </p>
+    <h3 id="terms-of-service">Terms of Service</h3>
+    <p>
+      Yes, legalese is boring, but we must protect ourselves — and by extension, you and your data —
+      against unfriendly folks. We have a Terms of Service describing your (and our) behavior and
+      rights related to content, privacy, and laws. To use this service, you must agree to abide by
+      our TOS.
+    </p>
+    <h2 id="temporal-events">Temporal Events</h2>
+    <p>
+      Temporal is dedicated to providing a harassment-free experience for participants at all of our
+      events, whether they are held in person or virtually. Temporal events exist to encourage the
+      open exchange of ideas and expression and require an environment that recognizes the inherent
+      worth of every person and group. While at Temporal events or related ancillary or social
+      events, any participants, including members, speakers, attendees, volunteers, sponsors, and
+      anyone else, should not engage in harassment in any form.
+    </p>
+    <p>
+      This Code of Conduct may be revised at any time by Temporal and the terms are non-negotiable.
+      Your registration for or attendance at any Temporal event, whether it’s held in person or
+      virtually, indicates your agreement to abide by this policy and its terms.
+    </p>
+    <h3 id="expected-behavior">Expected Behavior</h3>
+    <p>
+      All event participants, whether they are attending an in-person event or a virtual event, are
+      expected to behave in accordance with professional standards, with both this Code of Conduct
+      as well as their respective employer’s policies governing appropriate workplace behavior and
+      applicable laws.
+    </p>
+    <h3 id="unaccepted-behavior">Unaccepted Behavior</h3>
+    <p>
+      Harassment will not be tolerated in any form, whether in person or virtually, including, but
+      not limited to, harassment based on gender, gender identity and expression, sexual
+      orientation, disability, physical appearance, body size, race, age, religion or any other
+      status protected by laws in which the conference or program is being held. Harassment includes
+      the use of abusive, offensive or degrading language, intimidation, stalking, harassing
+      photography or recording, inappropriate physical contact, sexual imagery and unwelcome sexual
+      advances or requests for sexual favors. Any report of harassment at one of our events, whether
+      in person or virtual, will be addressed immediately. Participants asked to stop any harassing
+      behavior are expected to comply immediately. Anyone who witnesses or is subjected to
+      unacceptable behavior should notify Temporal at once.
+    </p>
+    <p>
+      Individuals who participate (or plan to participate) in Temporal events, whether its an
+      in-person event or a virtual event, should conduct themselves at all times in a manner that
+      comports with both the letter and spirit of this policy prohibiting harassment and abusive
+      behavior, whether before, during or after the event. This includes statements made in social
+      media postings, on-line publications, text messages, and all other forms of electronic
+      communication.
+    </p>
+    <h3 id="consequences-of-unaccepted-behavior">Consequences of Unaccepted Behavior</h3>
+    <p>
+      If a participant engages in harassing behavior, whether in person or virtually, the event
+      organizers may take any action they deem appropriate depending on the circumstances, ranging
+      from issuance of a warning to the offending individual to expulsion from the conference with
+      no refund. Temporal reserves the right to exclude any participant found to be engaging in
+      harassing behavior from participating in any further Temporal events, trainings or other
+      activities.
+    </p>
+    <p>
+      If a participant (or individual wishing to participate in a Temporal event, in-person and/or
+      virtual), through postings on social media or other online publications or another form of
+      electronic communication, engages in conduct that violates this policy, whether before, during
+      or after a Temporal event, Temporal may take appropriate corrective action, which could
+      include imposing a temporary or permanent ban on an individual’s participation in future
+      Temporal events.
+    </p>
+    <h3 id="what">What</h3>
+    <p>To Do If You Witness or Are Subject to Unaccepted Behavior</p>
+    <p>
+      If you are being harassed, notice that someone else is being harassed, or have any other
+      concerns relating to harassment, please contact Temporal immediately at <a
+        href="mailto:conduct@temporal.io">conduct@temporal.io</a
+      > or using the Harassment Complaint Form button at the top of this page.
+    </p>
+  </div>
 </section>
