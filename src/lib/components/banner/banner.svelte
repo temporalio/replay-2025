99853d133f013047a40aa4a13b62e504415dcfa6
Katie Toler <katie.toler@temporal.io>
2025-02-21 11:59:34 -0700

    Add banner component and img per design

---
+++ src/lib/components/banner/banner.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<section
+  class="mt-10 flex flex-row items-center justify-center bg-green p-10 px-40 text-black max-md:flex-col max-md:px-20"
+>
+  <div class="flex flex-col">
+    <div class="font-jaro text-4xl uppercase max-md:text-2xl">ANNOUNCING SPECIAL GUESTS</div>
+    <div class="font-afacad text-5xl uppercase max-md:text-2xl">
+      The Tech Roast Show is coming to Replay for their first show in London
+    </div>
+  </div>
+  <div class="w-full min-w-[250px]">
+    <enhanced:img src="$assets/tech-roast.png" alt="Tech Roast" />
+  </div>
+</section>
